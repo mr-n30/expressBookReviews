97da99f07ac779751b2db7ebaaef40fd3eb3a623
mr-n30 <mr-n30@localhost>
2025-08-28 05:18:14 -0500

    Added auth middleware

--- final_project/index.js
+++ final_project/index.js
@@ -5,18 +5,30 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
+app.set('etag', false);
+const jwt_secret = 'SuperSecretK3y123@!$';
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (typeof req.session.authorization !== 'undefined') {
+        jwt.verify(req.session.authorization['accessToken'],
+            jwt_secret,
+            (err, user) => {
+                if (err) return res.status(401).json({ message: "You are not authenticated!" })
+                req.user = user
+                next()
+            })
+    }
+
+    return res.status(401).json({message: "You are not authenticated!2"})
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,26 +3,46 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = {
+  "admin": { password: "password" }
+};
+const jwt_secret = 'SuperSecretK3y123@!$';
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  if (typeof users[username] !== 'undefined') return true
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+  if (isValid(username) && users[username]["password"] === password) return true
+  return false
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body
+  if ([username, password].some(item => typeof item === 'undefined')) {
+    return res.send({ message: "Missing parameters!" })
+  } else if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign(
+      { data: username },
+      jwt_secret,
+      { expiresIn: 60 * 60 }
+    )
+    req.session.authorization = { accessToken, username }
+    return res.status(302).redirect('/')
+  } else {
+    return res.status(401).json({ message: "You are not authenticated" });
+  }
 });
+
+regd_users.get("/auth/home", (req, res) => {
+  return res.send("You are logged in!")
+})
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
