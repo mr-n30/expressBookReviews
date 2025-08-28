defa5455a76ab6607989ce2c088987bfdcfe471c
mr-n30 <mr-n30@localhost>
2025-08-28 04:11:58 -0500

    Revert "Added login functionality" still needs to set the session / JWT
    This reverts commit 53c86d041cc3a7e3b5d39d08cbd9e2552f9808ce.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,30 +3,26 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = {
-  "admin": { password: "password" }
-};
+let users = [];
 
-const isValid = (username) => { //returns boolean
-  //write code to check is the username is valid
+const isValid = (username)=>{ //returns boolean
+//write code to check is the username is valid
 }
 
-const authenticatedUser = (username, password) => { //returns boolean
-  //write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ //returns boolean
+//write code to check if username and password match the one we have in records.
 }
 
 //only registered users can login
-regd_users.post("/login", (req, res) => {
-  const { username, password } = req.body
-  if ([username, password].some(item => typeof item === 'undefined')) return res.send({ message: "Missing parameters!" })
-  if (users[username] && users[username]["password"] === password) return res.send("You are auth")
-  else return res.status(401).json({ message: "You are not authenticated" });
+regd_users.post("/login", (req,res) => {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.authenticated = regd_users;
