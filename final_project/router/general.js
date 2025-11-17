7d33a15188507528d59501d3680d4130c75585e2
mr-n30 <root@localhost>
2025-11-17 05:30:52 -0800

    Added register functionality

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,15 +4,25 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+// Register a user
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username
+  const password = req.body.password
+
+  if (!username || !password) {
+    return res.status(400).json({message: "Missing required parameter(s)!"})
+  }
+
+  if (isValid(username)) {
+    return res.status(403).json({message: "User already exists!"})
+  }
+
+  users[username] = {password}
+  return res.status(201).json({message: "User created!"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   return res.status(200).json({books});
 });
 
